6099de17574c42ed9f511426a2501085609db703
abraham-ukachi <abraham.ukachi@laplateforme.io>
2023-01-07 14:54:52 +0100

    Increase number of password characters

--- Database.php
+++ Database.php
@@ -281,7 +281,7 @@ class Database {
     return "CREATE TABLE IF NOT EXISTS " . $this->db_tablename . " (
       id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
       login VARCHAR(30) NOT NULL,
-      password VARCHAR(30) NOT NULL,
+      password VARCHAR(255) NOT NULL,
       email VARCHAR(50) NOT NULL,
       firstname VARCHAR(30) NOT NULL,
       lastname VARCHAR(30) NOT NULL
